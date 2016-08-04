9c4e7ad5b8da2029c6fe5034698c8781c9143d1c
JoMar Werdeman <JoMar Werdeman>
2016-08-04 13:49:08 -0700

    Initial commit

---
+++ app/src/main/java/com/discoget/app/hellozombie/MainActivity.java
@@ -0,0 +1,47 @@
+package com.discoget.app.hellozombie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView firstTextView = (TextView) findViewById(R.id.txtZombieStatus);
+
+        Button zombieButton = (Button) findViewById(R.id.btnZombie);
+
+        Button zapZombieButton = (Button) findViewById(R.id.zapZombieButton);
+
+        zombieButton.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View view) {
+                firstTextView.setText("No Zombies Found!");
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+    public void check_for_zombies(View view) {
+    }
+
+    public void change_text_field(View view) {
+
+        Toast.makeText(this,"Awesome",Toast.LENGTH_SHORT).show();
+
+
+    }
+}
