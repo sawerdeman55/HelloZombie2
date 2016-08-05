71f6f59fe8c36acaceb2e3ae7cf114c3e741cb97
sawerdeman55 <steven.werdeman@gmail.com>
2016-08-04 21:44:05 -0700

    simple changes

--- app/src/main/java/com/discoget/app/hellozombie/MainActivity.java
+++ app/src/main/java/com/discoget/app/hellozombie/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         zombieButton.setOnClickListener(new View.OnClickListener()  {
             @Override
             public void onClick(View view) {
-                firstTextView.setText("No Zombies Found!");
+                firstTextView.setText(R.string.zombieCmment);
             }
         });
 
